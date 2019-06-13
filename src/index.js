f141865b52b0429cb1d5a4ca85709e3e5db2c9c2
nevroje <joakim.berglind@gmail.com>
2019-06-13 16:55:29 +0200

    imported redux components to index, create data for reducer

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import cartReducer from './components/reducers/cartReducer';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-serviceWorker.unregister();
+const store = createStore(cartReducer)
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
