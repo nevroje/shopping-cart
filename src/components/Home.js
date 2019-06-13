88c2fd0b4b0c628b232aa2db4c467d149db5d5b5
nevroje <joakim.berglind@gmail.com>
2019-06-13 16:57:01 +0200

    created home component

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="container">
+        <h3>Home</h3>
+      </div>
+    );
+  }
+}
